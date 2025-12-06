17df46ac4bef1899ae13f3f3752f9aa0a9f98e51
Martijn Kooijman <martijn@MacBook-Pro-van-Martijn.local>
2025-12-06 16:45:59 +0100

    Added project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Quiz from "./components/quiz";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
+}
+
+export default App;
